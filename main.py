abcc2d92e49f1194591973a6baac536ae318a169
kf <drmartinlutherchang@gmail.com>
2019-10-20 22:41:28 -0700

    basic script to compile index page, run socket, and web server;
     On branch master
     Your branch is ahead of 'origin/master' by 1 commit.
       (use "git push" to publish your local commits)
    
     Changes to be committed:
    	new file:   main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+import sys, index, subprocess
+from memcache_socket import MemcacheSocket
+
+p = subprocess.Popen([sys.executable, './web_server.py'],
+                                    stdout=subprocess.PIPE,
+                                    stderr=subprocess.STDOUT)
+
+db_file = sys.argv[2] if len(sys.argv) == 3 else "database.sqlite"
+MemcacheSocket(db_file)
+
