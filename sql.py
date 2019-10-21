163f84f6dce869a9746ab899c10ea41e8bb5f196
kf <drmartinlutherchang@gmail.com>
2019-10-20 18:01:46 -0700

    add update and select * sql statements;
     On branch master
     Your branch is ahead of 'origin/master' by 1 commit.
       (use "git push" to publish your local commits)
    
     Changes to be committed:
    	modified:   sql.py

--- sql.py
+++ sql.py
@@ -2,7 +2,9 @@
 CREATE_TABLE = "CREATE TABLE IF NOT EXISTS memcache (key text unique, value text, flags integer)"
 
 INSERT_TABLE = "INSERT INTO memcache (key, value, flags) VALUES (?,?,?)"
+UPDATE_TABLE = "UPDATE memcache SET value=?, flags=? WHERE key=?";
 
+FETCH_TABLE  = "SELECT * FROM memcache"
 GET_TABLE    = "SELECT * FROM memcache where key = ?"
 
 DELETE_TABLE = "DELETE FROM memcache WHERE key = ?"
