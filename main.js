65be8c081a822744495580679046a43db2aee79f
kf <drmartinlutherchang@gmail.com>
2019-10-20 19:56:41 -0700

    vue component for memcache-row; handle click toggling visibility for value div;
     On branch master
     Your branch is ahead of 'origin/master' by 1 commit.
       (use "git push" to publish your local commits)
    
     Changes to be committed:
    	new file:   main.js

---
+++ main.js
@@ -0,0 +1,35 @@
+Vue.component('memcache-row', {
+
+	template: `
+		<div>
+			<div class="key" @click="setShowing"><slot name="key"></slot></div>
+			<div :class="{ 'hidden': !showing }"><slot name="value"></slot></div>
+		</div>`,
+
+	data() {
+
+		return {
+			showing: false
+		}
+
+	},
+
+	methods: {
+		
+		setShowing() {
+
+			console.log(this.showing);
+
+			this.showing = true;
+
+		}
+
+	}
+});
+
+new Vue({
+
+	el: '#root'
+
+});
+
