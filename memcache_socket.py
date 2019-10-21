b80332db2974ee4304b10fd7ef30cde3b82de7f3
kf <drmartinlutherchang@gmail.com>
2019-10-20 21:50:30 -0700

    add handle function to route set, get and delete requests;
     On branch master
     Your branch is ahead of 'origin/master' by 1 commit.
       (use "git push" to publish your local commits)
    
     Changes to be committed:
    	modified:   memcache_socket.py

--- memcache_socket.py
+++ memcache_socket.py
@@ -4,9 +4,11 @@ from memcache_db import MemcacheDB
 
 HOST = 'localhost'
 PORT = 11211
+BUFFER = 1024
+PROTOCOL = [ 'set', 'get', 'delete' ]
 
 class MemcacheSocket:
-    def __init__(self, db_file='memcache.sqlite'):
+    def __init__(self, db_file='database.sqlite'):
         self.db = MemcacheDB(db_file)
         self.init_sock()
         self.run()
@@ -26,15 +28,52 @@ class MemcacheSocket:
         self.db.clear(key)
 
     def handle(self, conn):
-        print "handler"
+        data = conn.recv(BUFFER)[::]
+        #check non empty
+        if not data:
+            resp = "CLIENT_ERROR empty input\r\n"
+        else:
+            req = data.split()
+            #check command exists
+            if req[0] not in PROTOCOL:
+                resp = "ERROR\r\n"
+            elif req[0] == 'get':
+                try:
+                    row = self.get(req[1])
+                    resp = "VALUE %s %i 0 %i\r\n%s\r\nEND\r\n" % (row[0], row[2], len(row[1]), row[1])
+                except KeyError:
+                    resp = "END\r\n"
+            elif req[0] == 'set':
+                if len(req) == 5:
+                    buffer_size = int(req[4])
+                    value = ""
+                    while buffer_size > 0:
+                        _BUFFER = buffer_size if buffer_size < BUFFER else BUFFER
+                        _value = conn.recv(_BUFFER)
+                        if not _value: break
+                        value += _value
+                        buffer_size -= BUFFER
+                    #clear newline
+                    conn.recv(BUFFER)
+                    self.set(req[1], value, int(req[2]))
+                    resp = "STORED\r\n"
+                else:
+                    resp = "CLIENT_ERROR insufficient arguments\r\n"
+            elif req[0] == 'delete':
+                try:
+                    row = self.delete(req[1])
+                    resp = "DELETED\r\n"
+                except KeyError:
+                    resp = "NOT_FOUND\r\n"
+        conn.send(resp)
 
     def run(self):
         conn, addr = self.sock.accept()
-        print "connection from: " + addr
 	while 1:
 	    try:
 		self.handle(conn)
 	    except socket.error as err:
+                print err
 	        return self.close()
         self.close()
 
@@ -42,3 +81,4 @@ class MemcacheSocket:
         self.db.end()
         self.sock.close()
         self.sock = None
+
