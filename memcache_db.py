837a7664a9d58735458b29221cd868805f31c070
kf <drmartinlutherchang@gmail.com>
2019-10-20 17:59:11 -0700

    add keys, and fetch methods; add update for insertion to update values if key exists; raise key error on deletion of missing key; resolve issue with get response data;
     On branch master
     Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    	modified:   memcache_db.py

--- memcache_db.py
+++ memcache_db.py
@@ -10,19 +10,40 @@ class MemcacheDB:
                 #cursor
 		self.cur = self.conn.cursor()
 
+	def keys(self):
+		""" return a generator of all keys """
+		cursor = self.conn.cursor()
+		for row in cursor.execute(FETCH_TABLE):
+			yield row[0]
+
         def insert(self, key, value, flag):
-            """ insert row into me db """
-            self.cur.execute(INSERT_TABLE, (key, value, flag))
-            self.cur.commit()
+            """ insert (or update) db row """
+            if key not in self.keys():
+		    #if key doesn't exist create
+		    self.cur.execute(INSERT_TABLE, (key, value, flag))
+            else:
+		    #update the key's value
+                    #memcache 'set' overwrites values
+		    self.cur.execute(UPDATE_TABLE, (value, flag, key))
+            self.conn.commit()
 
         def get(self, key):
             """ retrieve row data associated with key """
             self.cur.execute(GET_TABLE, (key, ))
             row = self.cur.fetchone()
-            return row
+            return row[0], row[1], row[2]
+
+	def fetch(self):
+		""" return generator for rows in memcache """
+		cursor = self.conn.cursor()
+		for row in cursor.execute(FETCH_TABLE):
+			yield row[0], row[1], row[2]
 
         def clear(self, key):
             """ remove a row from db """
+	    #check key exists
+	    if key not in self.keys():
+	        raise KeyError(key)
 	    self.cur.execute(DELETE_TABLE, (key, ))
 	    self.conn.commit()
 
